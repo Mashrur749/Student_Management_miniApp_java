211bbc45b79d6478f5976b033552c6253c28ee9a
Musaddiqur Rahman <mashrur749@gmail.com>
2019-05-10 17:33:39 -0400

    setStudentId() added

--- src/StudentDatabaseApp/Student.java
+++ src/StudentDatabaseApp/Student.java
@@ -6,10 +6,11 @@ public class Student {
     private String firstName;
     private String lastName;
     private int gradeYear;
-    private int studentId;
+    private String studentId;
     private String courses;
     private int tuitionBalance;
     private int costOfCoutse = 600;
+    private static int id = 1000;
     //constructor prompt user to enter student's name and year
     public Student (){
         Scanner in  = new Scanner(System.in);
@@ -25,11 +26,17 @@ public class Student {
                 "Enter student class level:");
         this.gradeYear = in.nextInt();
 
-        System.out.println(firstName+ " " + lastName+ " " + gradeYear);
+        this.setStudentId();
 
+        System.out.println(firstName+ " " + lastName+ " " + gradeYear + " " + studentId );
     }
-    //generate an ID
 
+    //generate an ID
+    private void setStudentId(){
+        //Grade Level + id
+        id++;
+        this.studentId = this.gradeYear + "" +id;
+    }
     //enroll in courses
 
     //view balance
