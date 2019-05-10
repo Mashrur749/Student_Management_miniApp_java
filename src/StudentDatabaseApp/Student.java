f2ed83a54dd20423694327bd607a14bc0815db84
Musaddiqur Rahman <mashrur749@gmail.com>
2019-05-10 17:05:04 -0400

    initial documentation

---
+++ src/StudentDatabaseApp/Student.java
@@ -0,0 +1,23 @@
+package StudentDatabaseApp;
+
+public class Student {
+    private String firstName;
+    private String lastName;
+    private String gradeYear;
+    private int studentId;
+    private String courses;
+    private int tuitionBalance;
+    private int costOfCoutse = 600;
+    //constructor prompt user to enter student's name and year
+
+    //generate an ID
+
+    //enroll in courses
+
+    //view balance
+
+    //pay tuition
+
+    //show status
+
+}
---
+++ src/StudentDatabaseApp/StudentDatabaseApp.java
@@ -0,0 +1,11 @@
+package StudentDatabaseApp;
+
+public class StudentDatabaseApp {
+    public static void main(String[] args) {
+        //ask how many new users to add
+
+        //create n number of new students
+
+
+    }
+}
